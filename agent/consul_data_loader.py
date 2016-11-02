2cdd935bc73686e02ef13fe44c834999d3dc978d
Filip Sobczak <filsob@gmail.com>
2016-11-02 16:27:40 +0000

    Update data loader

--- agent/consul_data_loader.py
+++ agent/consul_data_loader.py
@@ -4,39 +4,52 @@ import key_naming_convention
 import logging
 from consul_api import ConsulError
 from server_role import ServerRole
+from actions import InstallAction, UninstallAction
 from service import Service
 
 class ConsulDataLoader:
     def __init__(self, consul_api):
         self._consul_api = consul_api
 
+    def _load_service(self, environment, deployment_id, name, version):
+        definition_key = key_naming_convention.get_service_definition_key(environment, name, version)
+        installation_key = key_naming_convention.get_service_installation_key(environment, name, version)
+        definition = self._consul_api.get_value(definition_key).get('Service', {})
+        definition['Address'] = environment.ip_address
+        installation = self._consul_api.get_value(installation_key)
+        return Service(definition, installation)
+
     def load_server_role(self, environment):
         server_role = ServerRole(environment.server_role)
         services_key = key_naming_convention.get_server_role_services_key(environment)
         for key in self._consul_api.get_keys(services_key):
             try:
-                service_target_state = self._consul_api.get_value(key)
-                service_name = service_target_state.get('Name')
-                service_version = service_target_state.get('Version')
-                deployment_id = service_target_state.get('DeploymentId')
-                deployment_slice = service_target_state.get('Slice','none')
+                definition = self._consul_api.get_value(key)
+                name = definition.get('Name')
+                version = definition.get('Version')
+                deployment_id = definition.get('DeploymentId')
+                deployment_slice = definition.get('Slice','none')
+                deployment_action = definition.get('Action', 'Install')
 
-                service_definition_key = key_naming_convention.get_service_definition_key(environment, service_name, service_version)
-                service_installation_key = key_naming_convention.get_service_installation_key(environment, service_name, service_version)
-                service_definition = self._consul_api.get_value(service_definition_key).get('Service', {})
-                service_definition['Address'] = environment.ip_address
-                service_installation = self._consul_api.get_value(service_installation_key)
+                if deployment_action not in ['Install', 'Uninstall']:
+                    logging.warning('Unknown deployment action \'{0}\', will ignore it.'.format(deployment_action))
 
-                service = Service(service_definition, service_installation)
-                service.deployment_id = deployment_id
-                service.slice = deployment_slice
-                service.tag('deployment_id:', deployment_id)
-                service.tag('server_role:', environment.server_role)
-                service.tag('slice:', deployment_slice)
-                server_role.services[deployment_id] = service
+                service = self._load_service(environment, deployment_id, name, version)
+
+                # service.deployment_id = deployment_id
+                # service.tag('deployment_id:', deployment_id)
+                # service.tag('server_role:', environment.server_role)
+                # service.tag('slice:', deployment_slice)
+                # server_role.services[deployment_id] = service
+
+                if deployment_action == 'Install':
+                    server_role.actions.append(InstallAction(deployment_id, service))
+                elif deployment_action == 'Uninstall':
+                    server_role.actions.append(UninstallAction(deployment_id, service))
+
             except (ConsulError, ValueError) as e:
                 logging.exception(e)
-                logging.warning('Failed to read service from Consul, will ignore. [name: %s version: %s deployment_id: %s]', service_name, service_version, deployment_id)
+                logging.warning('Failed to read service from Consul, will ignore. [name: {0} version: {1} deployment_id: {2}]'.format(name, version, deployment_id))
         return server_role
 
     def load_service_catalogue(self):
