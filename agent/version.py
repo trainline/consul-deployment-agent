b039f23f4527949c87cfc27cb7e6b235c9004e65
Duncan Hall <duncanhall@users.noreply.github.com>
2017-03-15 10:32:50 +0000

    Bump version

--- agent/version.py
+++ agent/version.py
@@ -1,3 +1,3 @@
 # Copyright (c) Trainline Limited, 2016-2017. All rights reserved. See LICENSE.txt in the project root for license information.
 
-semantic_version = '0.22.8'
+semantic_version = '0.22.9'
