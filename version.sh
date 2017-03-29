c81864218166e23f5f970ae73109817419a735c3
Duncan Hall <himself@duncanhall.net>
2017-03-29 14:56:05 +0100

    Generate build version & pass target to build

---
+++ version.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -euf
+
+GIT_INFO=$(git describe)
+TAG=$(echo $GIT_INFO | cut -d'-' -f1)
+SHA=$(echo $GIT_INFO | cut -d'-' -f3)
+
+if [[ "$TAG" == "$SHA" ]]; then
+  BUILD_VERSION="$TAG"
+else
+  BUILD_VERSION="$TAG-$BUILD_VERSION.${SHA}"
+fi
+
+echo "##teamcity[buildNumber '${BUILD_VERSION}']"
+
