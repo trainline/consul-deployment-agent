57a06e737d9d48da654d8daccf16265b4f4d7f14
Duncan Hall <himself@duncanhall.net>
2017-04-04 10:41:16 +0100

    Use correct env var method

--- set_version.py
+++ set_version.py
@@ -8,6 +8,6 @@ def write_version_file(version):
     with open('agent/version.py', 'w') as file:
         file.write(filedata)
 
-build_version = os.getenviron('BUILD_VERSION')
+build_version = os.getenv('BUILD_VERSION')
 write_version_file(build_version)
 
