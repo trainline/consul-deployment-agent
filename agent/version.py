d1fbf4ed5e1590445160ca5b54b67d1f5467a16f
Duncan Hall <himself@duncanhall.net>
2017-05-03 14:40:18 +0100

    Reinstate version

--- agent/version.py
+++ agent/version.py
@@ -1,3 +1,4 @@
 # Copyright (c) Trainline Limited, 2016-2017. All rights reserved. See LICENSE.txt in the project root for license information.
 
 # Correct version will be written as part of build process, local builds will show 0.0.0
+semantic_version = "0.0.0"
