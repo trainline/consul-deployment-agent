d070e38b0b9f090f5d4af94c636f89b9cc5d99e5
jake-cross <jake.cross@thetrainline.com>
2019-01-31 09:09:39 +0000

    add validate shell scripts to health checks

---
+++ config/skel/linux/healthchecks/consul/validate-service.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -xe
+
+echo "Verifying the installation"
+
+url="https://127.0.0.1:{{TTL_SERVICE_PORT}}/diagnostics/healthcheck"
+echo "Service URL=$url"
+
+max_retries=20
+retries=0
+
+while [[ $retries -lt $max_retries ]]
+do
+	if curl -ik $url | grep "OK"; then
+		echo "Success!"
+ 		exit 0
+	fi
+	sleep 4
+	((retries++))
+done
+
+echo "max retries ($max_retries) reached, installation check failed"
+exit 1
