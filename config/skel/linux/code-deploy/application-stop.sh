d2b614ff05978ed9dd76380df01e807aefdcae78
jake-cross <jake.cross@thetrainline.com>
2019-01-31 08:20:06 +0000

    update scripts to use templates from conventional branch

--- config/skel/linux/code-deploy/application-stop.sh
+++ config/skel/linux/code-deploy/application-stop.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
-if systemctl is-active test-orchestrator | grep "^active$"; then
-  systemctl stop test-orchestrator
+set -xe
+
+. $DEPLOYMENT_BASE_DIR/code-deploy/environment.sh
+
+if systemctl is-active $TTL_SERVICE_NAME_WITH_SLICE | grep "^active$"; then
+  systemctl stop $TTL_SERVICE_NAME_WITH_SLICE
 fi
+
--- config/skel/linux/code-deploy/on-after-install.sh
+++ config/skel/linux/code-deploy/on-after-install.sh
@@ -1,47 +1,84 @@
 #!/usr/bin/env bash
 
+set -xe
+
+. $DEPLOYMENT_BASE_DIR/code-deploy/environment.sh
+
+replace_env_vars() {
+  local TARGET_FILE=$1
+  sed -i "s/{{TTL_SERVICE_EXE}}/${TTL_SERVICE_EXE}/g" $TARGET_FILE
+  sed -i "s/{{TTL_IAM_ROLE}}/${TTL_IAM_ROLE}/g" $TARGET_FILE
+  sed -i "s/{{TTL_SERVICE_PORT}}/${TTL_SERVICE_PORT}/g" $TARGET_FILE
+  sed -i "s/{{TTL_SERVICE_SLICE}}/${TTL_SERVICE_SLICE}/g" $TARGET_FILE
+  sed -i "s/{{TTL_SERVICE_VERSION}}/${TTL_SERVICE_VERSION}/g" $TARGET_FILE
+  sed -i "s/{{TTL_SERVICE_CONSUL_NAME}}/${TTL_SERVICE_CONSUL_NAME}/g" $TARGET_FILE
+  sed -i "s/{{TTL_SERVICE_NAME}}/${TTL_SERVICE_NAME}/g" $TARGET_FILE
+  sed -i "s/{{TTL_SERVICE_NAME_WITH_SLICE}}/${TTL_SERVICE_NAME_WITH_SLICE}/g" $TARGET_FILE
+  sed -i "s/{{TTL_INSTANCE_ID}}/${TTL_INSTANCE_ID}/g" $TARGET_FILE
+  sed -i "s/{{TTL_ENVIRONMENT}}/${TTL_ENVIRONMENT}/g" $TARGET_FILE
+  sed -i "s/{{TTL_ENVIRONMENT_TYPE}}/${TTL_ENVIRONMENT_TYPE}/g" $TARGET_FILE
+}
+
+copy_certificates() {
+  echo "Copying certificates" >&2
+
+  local SRC_FILE=$TTL_INSTALL_SRC_DIR/certificate.pfx
+  local TARGET_FILE=/etc/$TTL_SERVICE_NAME.pfx
+
+  cp -f $SRC_FILE $TARGET_FILE
+  chmod 644 $TARGET_FILE
+  chown root.root $TARGET_FILE
+}
+
 create_systemd_unit_file() {
   echo "Creating systemd unit file" >&2
 
-  local SRC_FILE=/tmp/test-orchestrator/misc/test-orchestrator.service
-  local TARGET_FILE=/lib/systemd/system/test-orchestrator.service
+  local SRC_FILE=$TTL_INSTALL_SRC_DIR/misc/service.service
+  local TARGET_FILE=/lib/systemd/system/$TTL_SERVICE_NAME_WITH_SLICE.service
   
   cp -f $SRC_FILE $TARGET_FILE
   chmod 644 $TARGET_FILE
   chown root.root $TARGET_FILE
+  replace_env_vars $TARGET_FILE
 }
 
 copy_source_files() {
   echo "Copying source files" >&2
 
-  local SRC_DIR=/tmp/test-orchestrator
-  local TARGET_DIR=/opt/test-orchestrator
+  local SRC_DIR=$TTL_INSTALL_SRC_DIR
+  local TARGET_DIR=/opt/$TTL_SERVICE_NAME_WITH_SLICE
+
+  echo $SRC_DIR
+  echo $TARGET_DIR
   
   rm -rf $TARGET_DIR
   mkdir $TARGET_DIR
   cp -rf $SRC_DIR/* $TARGET_DIR
+  chmod +x $TARGET_DIR/code-deploy/*.sh
 }
 
 create_environment_file() {
   echo "Creating config file" >&2
 
-  local SRC_FILE=/tmp/test-orchestrator/misc/test-orchestrator.env
-  local TARGET_FILE=/etc/test-orchestrator.env
+  local SRC_FILE=$TTL_INSTALL_SRC_DIR/misc/service.env
+  local TARGET_FILE=/etc/$TTL_SERVICE_NAME_WITH_SLICE.env
   
   cp -f $SRC_FILE $TARGET_FILE
+  find $TTL_INSTALL_SRC_DIR -name '*.env' ! -name 'service.env' -exec cat {} \; >> /etc/$TTL_SERVICE_NAME_WITH_SLICE.env
   chmod 644 $TARGET_FILE
   chown root.root $TARGET_FILE
-
-  sed -i "s/{{TTL_ENVIRONMENT}}/${TTL_ENVIRONMENT}/g" $TARGET_FILE
-  sed -i "s/{{TTL_ENVIRONMENT_TYPE}}/${TTL_ENVIRONMENT_TYPE}/g" $TARGET_FILE
+  replace_env_vars $TARGET_FILE
 }
 
-
+copy_certificates
 copy_source_files
 create_environment_file
 create_systemd_unit_file
+replace_env_vars $TTL_INSTALL_SRC_DIR/healthchecks/consul/healthchecks.yml
+replace_env_vars $TTL_INSTALL_SRC_DIR/healthchecks/consul/validate-service.sh
+replace_env_vars $TTL_INSTALL_SRC_DIR/healthchecks/sensu/healthchecks.yml
+replace_env_vars $TTL_INSTALL_SRC_DIR/healthchecks/sensu/validate-service.sh
 
-cd /opt/test-orchestrator
-
-echo "Allowing execution of test-orchestrator start script" >&2
-chmod a+x /opt/test-orchestrator/src/TestOrchestrator.Web
+cd /opt/$TTL_SERVICE_NAME_WITH_SLICE
+echo "Allowing execution of $TTL_SERVICE_NAME start script" >&2
+chmod a+x /opt/$TTL_SERVICE_NAME_WITH_SLICE/$TTL_SERVICE_EXE
