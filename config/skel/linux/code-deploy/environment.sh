51b80d9bced409ef524edb3abbba1a5962426ed4
trainb0t <36226889+trainb0t@users.noreply.github.com>
2019-02-07 14:22:38 +0000

    Update environment.sh
    The logic for composing $TTL_SERVICE_SLICE was wrong, fixed

--- config/skel/linux/code-deploy/environment.sh
+++ config/skel/linux/code-deploy/environment.sh
@@ -11,7 +11,7 @@ export TTL_SERVICE_CONSUL_NAME=$EM_SERVICE_NAME
 IFS='-' read -ra SERVICE_NAME_PARTS <<< $EM_SERVICE_NAME
 export TTL_SERVICE_NAME=${SERVICE_NAME_PARTS[1]}
 
-if [ ! "$TTL_SERVICE_SLICE" == "none" ]; then
+if [ "$TTL_SERVICE_SLICE" == "none" ]; then
 	export TTL_SERVICE_NAME_WITH_SLICE=$(echo "$TTL_SERVICE_NAME" | tr '[:upper:]' '[:lower:]')
 else
 	export TTL_SERVICE_NAME_WITH_SLICE=$(echo "$TTL_SERVICE_NAME-$TTL_SERVICE_SLICE" | tr '[:upper:]' '[:lower:]')
