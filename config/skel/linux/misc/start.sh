e5bb276087453edccd7a4e18bc3d228f7b6ca850
Wojciech Kotlarski <wojciech.kotlarski@thetrainline.com>
2019-02-19 14:22:47 +0000

    Updated start.sh to handle spaces in env variables

--- config/skel/linux/misc/start.sh
+++ config/skel/linux/misc/start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 # Decrypts secret.env and passes the values as environment variables to the service
-env $(tlcrypt -d < /opt/{{TTL_SERVICE_NAME_WITH_SLICE}}/secret.env) /opt/{{TTL_SERVICE_NAME_WITH_SLICE}}/{{TTL_SERVICE_EXE}}
+eval export `cat /opt/{{TTL_SERVICE_NAME_WITH_SLICE}}/secret.env | tlcrypt -d`
+/opt/{{TTL_SERVICE_NAME_WITH_SLICE}}/{{TTL_SERVICE_EXE}}
